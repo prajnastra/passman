332cc274bb6183ff67edbe113a91eb99ad55610b
prajnastra <coder.abhijit90@gmail.com>
2023-03-07 18:08:22 +0530

    add: command line args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+use clap::Parser;
 use rand::{distributions::Alphanumeric, Rng};
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value_t = 5)]
+    length: usize,
+}
+
 fn main() {
-    let s = generate_pass(10);
+    let args = Args::parse();
+    let s = generate_pass(args.length);
 
     println!("Password: {}", s);
 }
